e271d2769a0a5a0e9c5af5d878bfbd6bf0ed6a0a
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-08-31 16:48:43 -0300

    fix: add NoArgsContructor for user and Transaction

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -12,6 +10,7 @@ import java.math.BigDecimal;
 @Table(name="users")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
@@ -29,4 +28,15 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.userType = data.userType();
+        this.email = data.email();
+        this.password = data.password();
+        this.balance = data.balance();
+    }
+
 }
